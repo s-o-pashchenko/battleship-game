473c31d56eca91d34ec84e637cb249b667e2329e
Serhii Pahschenko <s.o.pashchenko@gmail.com>
2024-01-17 13:49:33 -0600

    it works but ships generate incorrectly

---
+++ ships/src/App.js
@@ -0,0 +1,135 @@
+import logo from './logo.svg';
+import './App.css';
+
+//class for field
+class Field {
+  constructor(id, ship) {
+    this.id = id;
+    this.ship = ship
+  }
+}
+//class for row
+class Row {
+  constructor(rowId) {
+    this.rowId = rowId;
+    this.array = this.createRow(this.rowId);
+  }
+  createRow() {
+    let arr = []
+    for (let i = 0; i < 10; i++) {
+      arr.push(new Field((this.rowId + '' + i), 0));
+    }
+    return arr;
+  }
+  placeShip(ship, x) {
+    this.array[x].ship = ship;
+    return this.array[x].id;
+  }
+}
+
+let playMap = []
+
+for (let i = 0; i < 10; i++) {
+  playMap.push(new Row(i));
+}
+// generator of ships
+const randomCoords = {
+  ships: {
+    busy: [], // here is all ships coords
+    large: [],
+    medium: [],
+    small: [],
+    individual: []
+  },
+  shipsToGen() {
+    this.generateShip(4);
+    this.generateShip(3);
+    this.generateShip(3);
+    this.generateShip(2);
+    this.generateShip(2);
+    this.generateShip(2);
+    this.generateShip(1);
+    this.generateShip(1);
+    this.generateShip(1);
+    this.generateShip(1);
+  },
+  initial() {
+    return { x: Math.floor(Math.random() * 10), y: Math.floor(Math.random() * 10), d: Math.floor(Math.random() * 2) };
+  },
+  generateShip(size) {
+    const shipReady = this.check(size);
+    this.ships.busy.push(shipReady)
+  },
+  check(size) {
+    const shipToCheck = this.ship(size)
+    for (let i = 0; i < this.ships.busy.length; i++) {
+      for (let j = 0; j < size; j++) {
+        if (this.ships.busy[i].toString() == shipToCheck[j].toString()) {
+          return true
+        } else {
+          return shipToCheck;
+        }
+      }
+    }
+    return shipToCheck;
+  },
+  ship(size) {
+    const startPos = this.initial();
+    let ship = [];
+    if (startPos.d == 1) { // Horizontal
+      if (startPos.x < 4) {
+        for (let i = 0; i < size; i++) {
+          ship.push([startPos.x, startPos.y]);
+          startPos.x++;
+        }
+      } else {
+        for (let i = 0; i < size; i++) {
+          ship.push([startPos.x, startPos.y]);
+          startPos.x--;
+        }
+      }
+      return ship;
+    } else {
+      if (startPos.x < 4) {
+        for (let i = 0; i < size; i++) {
+          ship.push([startPos.x, startPos.y]);
+          startPos.y++;
+        }
+      } else {
+        for (let i = 0; i < size; i++) {
+          ship.push([startPos.x, startPos.y]);
+          startPos.y--;
+        }
+      }
+      return ship;
+    }
+  }
+}
+
+randomCoords.shipsToGen()
+
+function changeTable() {
+  const ship = randomCoords.ships.busy;
+  for (let i = 0; i < ship.length; i++) {
+    for (let j = 0; j < ship[i].length; j++) {
+      playMap[ship[i][j][0]].array[ship[i][j][1]].ship = 1;
+    }
+  }
+}
+
+changeTable();
+
+console.log(randomCoords.ships.busy);
+console.log(playMap);
+
+function App() {
+  return (
+    <div>
+      <table class="mx-auto">
+        {playMap.map(row => <tr id={row.rowId}>{row.array.map(field => <td id={field.id} class="w-10 h-10 border-2">{field.ship}</td>)}</tr>)}
+      </table>
+    </div>
+  );
+}
+
+export default App;
